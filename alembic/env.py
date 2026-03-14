e146d9e30b998df42364cef7f88740a28e709508
Madhur Goel <madhur12a16072@gmail.com>
2026-03-14 17:01:24 +0530

    Configs

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,13 @@ from app.database.models import Base  # noqa: F401 - imports trigger model regis
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+from app.core.config import settings
 
+# Override alembic.ini DB URL with app config
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url.replace("+asyncpg", "")
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
